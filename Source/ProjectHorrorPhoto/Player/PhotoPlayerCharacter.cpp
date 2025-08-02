1a6a034eace1ee33c84943e35a4a7901596ad95e
Tymur Slynko <timurslinko1@gmail.com>
2025-08-02 21:01:00 +0300

    Initial Commit

---
+++ Source/ProjectHorrorPhoto/Player/PhotoPlayerCharacter.cpp
@@ -0,0 +1,32 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "PhotoPlayerCharacter.h"
+
+
+// Sets default values
+APhotoPlayerCharacter::APhotoPlayerCharacter()
+{
+	// Set this character to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+}
+
+// Called when the game starts or when spawned
+void APhotoPlayerCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APhotoPlayerCharacter::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+}
+
+// Called to bind functionality to input
+void APhotoPlayerCharacter::SetupPlayerInputComponent(UInputComponent* PlayerInputComponent)
+{
+	Super::SetupPlayerInputComponent(PlayerInputComponent);
+}
+
