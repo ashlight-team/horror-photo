7ea4975e861afa6d3b5112bb2b80f6c1e5938e00
Tymur Slynko <timurslinko1@gmail.com>
2025-08-02 21:06:40 +0300

    Something

--- Source/ProjectHorrorPhoto/ProjectHorrorPhoto.Build.cs
+++ Source/ProjectHorrorPhoto/ProjectHorrorPhoto.Build.cs
@@ -10,7 +10,7 @@ public class ProjectHorrorPhoto : ModuleRules
 	
 		PublicDependencyModuleNames.AddRange(new string[] { "Core", "CoreUObject", "Engine", "InputCore", "EnhancedInput" });
 
-		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities",  "EnhancedInput", "GameplayTags", "CommonUI" });
+		PrivateDependencyModuleNames.AddRange(new string[] { "GameplayAbilities",  "EnhancedInput", "GameplayTags", "CommonUI", "GameplayTasks"});
 
 		// Uncomment if you are using Slate UI
 		// PrivateDependencyModuleNames.AddRange(new string[] { "Slate", "SlateCore" });
